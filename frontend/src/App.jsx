3587bc57bf7cefe2b2733da838d0eef874c1f783
Sumit5089 <sumitgaikwad930@gmail.com>
2024-07-06 02:07:56 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-400">
-      Hello world!
-    </h1>
-    </div>
+    <>
+     <Navbar />
+    </>
   )
 }
 
