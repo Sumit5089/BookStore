17a2814baf37ee5476b831721604ab352aeb7003
Sumit5089 <sumitgaikwad930@gmail.com>
2024-07-06 17:59:44 +0530

    sigup login validations

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm} from "react-hook-form";
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
+    
+      const onSubmit = (data) => console.log(data)
   return (
     <>
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
-          <form method="dialog">
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <Link
+             to="/" 
+             className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" 
+             onClick={() => document.getElementById("my_modal_3").close()}>
               ✕
             </Link>
-          </form>
+          
           <h3 className="font-bold text-lg">Login</h3>
           <div className="mt-4 space-y-2">
             <span className="px-3">Email</span>
@@ -20,7 +31,10 @@ const Login = () => {
               type="email"
               placeholder="Enter Your Email"
               className="w-80 px-3 py-1 border rounded-md outline-none"
+              {...register("email", { required: true })}
             />
+            <br />
+             {errors.email && <span className="text-sm text-red-500">This field is required</span>}
           </div>
           <br />
           <div className="mt-4 space-y-2">
@@ -30,7 +44,10 @@ const Login = () => {
               type="password"
               placeholder="Enter Your Password"
               className="w-80 px-3 py-1 border rounded-md outline-none"
+              {...register("password", { required: true })}
             />
+            <br />
+             {errors.password && <span className="text-sm text-red-500">This field is required</span>}
           </div>
           <div className="flex justify-around mt-4">
             <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">Login</button>
@@ -39,6 +56,7 @@ const Login = () => {
               {""}
             </p>
           </div>
+          </form>
         </div>
       </dialog>
     </>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,62 +1,105 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Login from './Login';
+import React from "react";
+import { Link } from "react-router-dom";
+import Login from "./Login";
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
     <>
-        <div className='flex h-screen items-center justify-center '>
-        <div className='w-[500px]'>
-        <div className='modal-box'>
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </Link>
-          </form>
-          <h3 className="font-bold text-lg">Signup</h3>
-          <div className="mt-4 space-y-2">
-            <span className="px-3">Username</span>
-            <br />
-            <input
-              type="text"
-              placeholder="Enter Username"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          <br />
-          <div className="mt-4 space-y-2">
-            <span className="px-3">Email</span>
-            <br />
-            <input
-              type="email"
-              placeholder="Enter Your Email"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          <br />
-          <div className="mt-4 space-y-2">
-            <span className="px-3">Password</span>
-            <br />
-            <input
-              type="password"
-              placeholder="Enter Your Password"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          <div className="flex justify-around mt-4">
-            <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">Signup</button>
-            <p className="mt-3">
-              Have Account? <button to="/" className="px-5 underline text-blue-500 cursor-pointer" onClick={() => document.getElementById("my_modal_3").showModal()}>Login</button>
-              {""}
-              <Login/>
-            </p>
+      <div className="flex h-screen items-center justify-center ">
+        <div className="w-[500px]">
+          <div className="modal-box">
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <Link
+                to="/"
+                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              >
+                ✕
+              </Link>
+
+              <h3 className="font-bold text-lg">Signup</h3>
+              <div className="mt-4 space-y-2">
+                <span className="px-3">Username</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Enter Username"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                  {...register("username", { required: true })}
+                />
+                <br />
+                {errors.username && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <br />
+              <div className="mt-4 space-y-2">
+                <span className="px-3">Email</span>
+                <br />
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                  {...register("email", { required: true })}
+                />
+                <br />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <br />
+              <div className="mt-4 space-y-2">
+                <span className="px-3">Password</span>
+                <br />
+                <input
+                  type="password"
+                  placeholder="Enter Your Password"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                  {...register("password", { required: true })}
+                />
+                <br />
+                {errors.password && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div className="flex justify-around mt-4">
+                <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                  Signup
+                </button>
+                <p className="mt-3">
+                  Have Account?{" "}
+                  <button
+                    to="/"
+                    className="px-5 underline text-blue-500 cursor-pointer"
+                    onClick={() =>
+                      document.getElementById("my_modal_3").showModal()
+                    }
+                  >
+                    Login
+                  </button>
+                  <Login /> {""}
+                </p>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-        </div>
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
