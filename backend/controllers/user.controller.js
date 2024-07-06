ab153a99744edbd587f5e9f3f9cbe3d0308fb636
Sumit5089 <sumitgaikwad930@gmail.com>
2024-07-06 21:23:48 +0530

    login route setup and controller setup

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,3 +14,25 @@ export const signup = async(req, res) =>{
         res.status(404).json({message: error.message});
     }
 }
+
+export const login = async(req, res) =>{
+    try{
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message: "User does not exist"});
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);    
+        if(!isPasswordCorrect) { 
+            return res.status(400).json({message: "Invalid credentials"});
+            res.status(200).json(user);
+        }else{
+            res.status(200).json({message: "Login successful", user:{
+                id: user._id,
+                username: user.username,
+                email: user.email
+            
+        }});
+        }
+    } catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
