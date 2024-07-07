d981b32611ebbb22c17205c47390ba04caaf8869
Sumit5089 <sumitgaikwad930@gmail.com>
2024-07-07 18:21:12 +0530

    responsive setting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import AuthProvider from './context/AuthProvider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <div className='dark:bg-slate-900 dark:text-white'>
+      <div>
        <App />
       </div>
     </AuthProvider>
